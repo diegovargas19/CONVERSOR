8309639f37919d7c9df1d4625a4df6e46de470a4
Diego19 <za210111209@zapopan.tecmm.edu.mx>
2023-03-02 22:32:02 -0600

    commit

--- Conversor.py
+++ Conversor.py
@@ -11,6 +11,40 @@ class Conversor:
     def __init__(self, raiz):#raiz = root
         raiz.title("Pies a metros")
 
+        self.pies = StringVar()
+        self.metros = StringVar()
+
+        mainFrame = ttk.Frame(raiz)
+        mainFrame.grid(column=0, row=0)
+        
+        piesEntry = ttk.Entry(mainFrame, textvariable=self.pies)
+        piesEntry.grid(column=1, row=0)
+
+        ttk.Label(mainFrame, text="pies").grid(column=2, row=0)
+        
+        ttk.Label(mainFrame, text="Son equivalentes a:").grid(column=0, row=1)
+
+        ttk.Label(mainFrame, textvariable=self.metros).grid(column=1, row=1)
+
+        ttk.Label(mainFrame, text="metros").grid(column=2, row=1)
+
+        ttk.Button(mainFrame, text="Calcular", command=self.calcular).grid(column=2, row=2)
+
+        #Hacer la operacion presionando <enter>
+        raiz.bind("<Return>", self.calcular)
+
+    def calcular(self, *args):
+         print("Boton presionado")  
+         piesUsuario = self.pies.get() #siempre devuelve una cadena.
+         
+        
+         print("Pies ingresados: ",self.pies.get())   
+         piesFlotante =float(piesUsuario)#con el float convierte la cadena a flotante.  
+         metros = piesFlotante / 3.281 
+         print("Metros: ", metros)
+         self.metros.set(metros)
+
+
 if __name__=="__main__":
     print("Este es el archivo principal.")
     print("Nada mas se mostrara este mensaje si es el principal")
